e094fbc491c4b5934e60319bf9403cacfce29a9a
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2020-05-23 21:36:20 +0800

    First Commit, Making some layout and adding some background

---
+++ app/src/main/java/com/example/emergencybutton/ForpassActivity.kt
@@ -0,0 +1,12 @@
+package com.example.emergencybutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForpassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forpass)
+    }
+}
